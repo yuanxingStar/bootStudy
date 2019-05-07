a46e3f00e42e329f8748423f26fdfa37bfa57497
yuanxing <18561703921@163.com>
2019-05-07 11:45:39 +0800

    1.创建父项目bootStudy 2.创建boot测试项目boot-first 3.创建boot公共依赖项目 boot-common

---
+++ bootStudy/boot-first/src/main/java/org/yuanxing/bootFirst/BootFirstApplication.java
@@ -0,0 +1,13 @@
+package org.yuanxing.bootFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootFirstApplication.class, args);
+    }
+
+}
---
+++ bootStudy/boot-first/src/test/java/org/yuanxing/bootFirst/BootFirstApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yuanxing.bootFirst;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootFirstApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
