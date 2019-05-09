68091e963e01fc8ef42cc5aef8c52a37dbf04068
yuanxing <18561703921@163.com>
2019-05-09 13:48:48 +0800

    boot 整合h2数据库

---
+++ bootStudy/boot-h2/src/main/java/org/yuanxing/booth2/BootH2Application.java
@@ -0,0 +1,13 @@
+package org.yuanxing.booth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootH2Application.class, args);
+    }
+
+}
