bf1c1549595ad5cb13c432e3db24a2cbb919c00b
yuanxing <18561703921@163.com>
2019-07-06 21:16:41 +0800

    no message

---
+++ boot-pay/src/main/java/org/yuanxing/bootpay/BootPayApplication.java
@@ -0,0 +1,13 @@
+package org.yuanxing.bootpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootPayApplication.class, args);
+    }
+
+}
---
+++ boot-pay/src/test/java/org/yuanxing/bootpay/BootPayApplicationTests.java
@@ -0,0 +1,16 @@
+package org.yuanxing.bootpay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootPayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
